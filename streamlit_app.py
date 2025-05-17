481a821d0af1dc1b5081bcb868751bae98c3fa8a
Yashaswi28 <yashaswigowda2803@gmail.com>
2025-05-17 12:59:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 # === Prediction Section ===
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-   st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
     with open("temp.jpg", "wb") as f:
         f.write(uploaded_file.getbuffer())
 
